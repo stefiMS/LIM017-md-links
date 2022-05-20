fefe0d20a9ada71a3d813c6ea370976970d9e88e
StefiMS <tefi_154@hotmail.com>
2022-05-19 23:41:39 -0500

    create function getOnlyFilesMD (array)

--- index.js
+++ index.js
@@ -25,21 +25,98 @@ const convertAbsolutePath = (routePath) => (absolutePath(routePath) ? routePath 
 // console.log('prueba');
 const readFiles = (routePath) =>  fs.readFileSync(routePath, 'utf-8');
 const determineFile = (routePath) => fs.statSync(routePath).isFile();
-const directoryNavigator = (routePath) => fs.readdirSync(routePath)
+const determineDirectory = (routePath) => fs.statSync(routePath).isDirectory();
+const readDirectory = (routePath) => fs.readdirSync(routePath);
+const fileExtension = (routePath) => path.extname(routePath)
 
+const getOnlyFilesMD = (routePath) => {
+  let arrayRouteFile = [];
+  const routeAbs = convertAbsolutePath(routePath);
+  if (determineDirectory(routeAbs/*routePath*/)){
+      const arrayDirectory = readDirectory(routeAbs/*routePath*/)
+      arrayDirectory.forEach((el) => {
+          const newRoute = path.join(routeAbs/*routePath*/, el)
+          if(determineFile(newRoute)){
+            fileExtension(newRoute) === '.md' ? arrayRouteFile.push(newRoute) : console.log('No es un archivo MD')
+          } else {
+            arrayRouteFile = arrayRouteFile.concat(getOnlyFilesMD(newRoute));
+          }
+       })
+  }else{
+    fileExtension(routeAbs/*routePath*/) === '.md' ? arrayRouteFile.push(routeAbs/*routePath*/) : console.log('No es un archivo MD')
+  }
+  return arrayRouteFile
+}
 
 // //ejemplo de ruta
 // const route = 'prueba/file1.md'
-//  const route = 'C:/Users/vladimir/Desktop/Stefani/LABORATORIA/LIM017-md-links/prueba'
+const route='prueba'
+//  const route = 'C:/Users/vladimir/Desktop/Stefani/LABORATORIA/LIM017-md-links/prueba/prueba.md'
 // console.log(routeExists(route))
 // console.log(absolutePath(route))
 // console.log(convertAbsolute(route))
 // console.log(readFiles(route))
 // console.log(determineFile(route))
-// console.log(directoryNavigator(route))
+// console.log(determineDirectory(route))
+// console.log(readDirectory(route))
+// console.log(fileExtension(route))
+console.log(getOnlyFilesMD(route))
+
 export {routeExists,
         absolutePath,
         convertAbsolutePath,
         readFiles,
         determineFile,
-        directoryNavigator }
+        determineDirectory,
+        readDirectory,
+        fileExtension,
+        getOnlyFilesMD }
+
+
+      //   export const getRouteWithPath = (pathRoute) => {
+      //     let arrayPaths = [];
+      //     // pathRoute is directory
+      //     if(isDirectory(pathRoute)) {
+      //     const arrayPathsDirectory = readDirectory(pathRoute)
+      //     arrayPathsDirectory.forEach((element) => {
+      //         const arrayArchives = getRouteWithPath(element)
+      //         // Uniendo array
+      //         arrayPaths.push(...arrayArchives)
+      //     });
+      //     } else {
+      //          console.log('Hola')
+      //     }
+      //     return arrayPaths
+      // }
+
+
+    //   export const getRouteFileAndDirectory = (pathRoute) => {
+    //     let arrayPaths = [];
+    //     // pathRoute is file
+    //     if(isFile(pathRoute)){
+    //         pullExtension(pathRoute) === '.md' ? arrayPaths.push(pathRoute) : console.log('No es un archivo MD')
+    //     } else if (isDirectory(pathRoute)){
+    //         //pathRoute is directory
+    //         const arrayPathsDirectory = readDirectory(pathRoute)
+    //         arrayPathsDirectory.forEach((element) => {
+    //           pullExtension(element) === '.md' ? arrayPaths.push(pathRoute) : console.log('No es un archivo/directorio MD')
+    //         })
+    //     };
+    //     return arrayPaths
+    // }
+    // export const getRouteDirectory = (pathRoute) => {
+    //     let arrayPaths = [];
+    //     if (isDirectory(pathRoute)){
+    //         const arrayPathsDirectory = readDirectory(pathRoute)
+    //         arrayPathsDirectory.forEach((element) => {
+    //             const routes = path.join(pathRoute, element)
+    //             if(isFile(routes)){
+    //                 pullExtension(routes) === '.md' ? arrayPaths.push(routes) : console.log('No es un archivo MD')
+    //             } else {
+    //                 arrayPaths = arrayPaths.concat(getRouteFileAndDirectory(routes));
+    //             }
+    //          })
+    //     };
+    //     return arrayPaths
+    // }
+
--- test/index.spec.js
+++ test/index.spec.js
@@ -3,51 +3,76 @@ import { routeExists,
   convertAbsolutePath,
   readFiles,
 determineFile,
-directoryNavigator} from '../index.js';
+determineDirectory,
+readDirectory,
+fileExtension,
+getOnlyFilesMD} from '../index.js';
 
-const testRouteAbs = 'C:\\Users\\vladimir\\Desktop\\Stefani\\LABORATORIA\\LIM017-md-links\\prueba\\prueba.md';
+const routeAbsolute = 'C:\\Users\\vladimir\\Desktop\\Stefani\\LABORATORIA\\LIM017-md-links\\prueba\\prueba.md';
 const routeRelative = 'prueba/prueba.md';
 const fileRoute = 'C:\\Users\\vladimir\\Desktop\\Stefani\\LABORATORIA\\LIM017-md-links\\prueba'
 const arrayFile = ['carpetaPrueba', 'file1.md', 'file2.html', 'file3.js', 'prueba.md'];
-// const routestest = 'C:\\Users\\vladimir\\Desktop\\Stefani\\LABORATORIA\\LIM017-md-links\\prueba\\file1.md'
 
+//test routeExists function
 describe('routeExists', () =>{
   it('should show existence the entered route',()=>{
-    expect(typeof routeExists(testRouteAbs)).toBe('boolean');
+    expect(typeof routeExists(routeAbsolute)).toBe('boolean');
   })
 })
 
+//test absolutePath function
 describe('absolutePath',() =>{
   it('should be an absolute path', ()=>{
-    expect(typeof absolutePath(testRouteAbs)).toBe('boolean');
+    expect(typeof absolutePath(routeAbsolute)).toBe('boolean');
   })
 })
 
+//test convertAbsolutePath function
 describe('convertAbsolutePath',() =>{
   it('should be an absolute path', ()=>{
-    expect(convertAbsolutePath(testRouteAbs)).toBe(testRouteAbs);
+    expect(convertAbsolutePath(routeAbsolute)).toBe(routeAbsolute);
   })
   it('should convert a relative path to an absolute path', ()=>{
-    expect(convertAbsolutePath(routeRelative)).toBe(testRouteAbs);
+    expect(convertAbsolutePath(routeRelative)).toBe(routeAbsolute);
   })
 })
 
+//test readfiles function
 describe('readFiles',() =>{
   it('should  read content of the file', ()=>{
     expect(readFiles(routeRelative)).toEqual('hola');
   })
 })
+
+//test determineFile function
 describe('determineFile',() =>{
   it('should be a file', ()=>{
-    expect(typeof determineFile(testRouteAbs)).toBe('boolean');
+    expect(typeof determineFile(routeAbsolute)).toBe('boolean');
   })
 })
-describe('directoryNavigator',()=>{
-  // it('should return an array',()=>{
-  //   expect(typeof directoryNavigator(arrayFile)).toBe('array')
-  // })
-  it('should show the content of a directory',() =>{
-    expect(directoryNavigator(fileRoute)).toEqual(arrayFile)
-  })
 
+//test determineDirectory function
+describe('determineDirectory',() =>{
+  it('should be a directory', ()=>{
+    expect(typeof determineDirectory(routeAbsolute)).toBe('boolean');
+  })
+  it('should be a directory',()=>{
+    expect(determineDirectory(fileRoute)).toEqual(true);
+  })
+  it('should not be a directory',()=>{
+    expect(determineDirectory(routeRelative)).toEqual(false);
+  })
+})
+
+//test readDirectory function
+describe('readDirectory',()=>{
+  it('should show the content of a directory',() =>{
+    expect(readDirectory(fileRoute)).toEqual(arrayFile)
+  })
+})
+
+describe('fileExtension',() =>{
+  it('should show the extension file', ()=>{
+    expect(fileExtension(routeAbsolute)).toBe('.md');
+  })
 })
